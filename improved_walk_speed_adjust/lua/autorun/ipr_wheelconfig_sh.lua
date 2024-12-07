e91ff8fd2f6da3d86d1bd843e60df4c3ec36bb26
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2024-12-07 03:47:35 +0100

    v1.3

--- improved_walk_speed_adjust/lua/autorun/ipr_wheelconfig_sh.lua
+++ improved_walk_speed_adjust/lua/autorun/ipr_wheelconfig_sh.lua
@@ -5,7 +5,7 @@
 ipr_SpeedWheel_Config = {}
 ipr_SpeedWheel_Config.MaxRotation = 10 --- The number of times you have to roll your mouse wheel to reach maximum speed (default:10).
 ipr_SpeedWheel_Config.AddKey = {true, --- Disabled mouse wheel with weapon selector but only if you press this key combination !
-    key = KEY_CAPSLOCK, ---Add an extra button in combination with the mouse wheel to activate dynamic walk speed.
+    key = KEY_CAPSLOCK, --- Add an extra button in combination with the mouse wheel to activate dynamic walk speed.
 }
 
 if (SERVER) then
@@ -13,5 +13,5 @@ if (SERVER) then
     ipr_SpeedWheel_Config.ReduceSlowWalkSpeed = 0.75 --- Withdraws 50% of max slow walk speed (set 1 if you don't want a speed reduction).
 else
     ipr_SpeedWheel_Config.DisableMWS = false --- Disable mouse wheel with weapon selector (you can still use the keyboard to navigate the weapon selector), if you also want to use only the wheel with this variable, set ipr_SpeedWheel_Config.AddKey to false !
-    ipr_SpeedWheel_Config.HUD = true ---Display hud
+    ipr_SpeedWheel_Config.HUD = true --- Display hud
 end
