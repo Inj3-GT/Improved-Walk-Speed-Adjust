b7ee65f23311c1ae34d9632195b37a791f434ded
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2024-12-07 09:08:55 +0100

    v1.5

--- improved_walk_speed_adjust/lua/autorun/ipr_wheelconfig_sh.lua
+++ improved_walk_speed_adjust/lua/autorun/ipr_wheelconfig_sh.lua
@@ -4,7 +4,7 @@
 ipr_SpeedWheel_Config = {}
 ipr_SpeedWheel_Config.MaxRotation = 10 ---  Le nombre de fois que vous devez faire tourner la molette de votre souris pour atteindre la vitesse maximale (par défaut : 10). | The number of times you have to roll your mouse wheel to reach maximum speed (default:10).
 ipr_SpeedWheel_Config.HUD = true --- Affichage du HUD et synchronisation de l'état (désactivez-le si vous voulez économiser des performances et éviter d'envoyer des messages réseaux). | Displayed HUD and synchronizes status (Disable it if you want to save performance and avoid sending network messages).
-ipr_SpeedWheel_Config.AddKey = {true, --- Désactive automatiquement la molette de la souris pour le sélecteur d'armes si vous appuyez sur la combinaison de touches (KEY_CAPSLOCK + Molette par défaut) ! | Disabled mouse wheel with weapon selector but only if you press this key combination !
+ipr_SpeedWheel_Config.AddKey = {true, --- Désactive automatiquement la molette de la souris pour le sélecteur d'armes lorsque vous appuyez sur la combinaison de touches (KEY_CAPSLOCK + Molette par défaut) ! | Disabled mouse wheel with weapon selector but only if you press this key combination !
     key = KEY_CAPSLOCK --- Ajoutez un bouton supplémentaire en combinaison avec la molette de la souris pour activer la vitesse de marche dynamique. | Add an extra button in combination with the mouse wheel to activate dynamic walk speed.
 }
 
@@ -15,5 +15,5 @@ if (SERVER) then
         msg = "Utilisez CAPSLOCK + Molette pour modifier la vitesse de votre personnage."
     }
 else
-    ipr_SpeedWheel_Config.DisableMWS = false --- Désactive la molette de la souris pour le sélecteur d'armes (vous pouvez toujours utiliser le clavier pour naviguer dans le sélecteur d'armes), si vous souhaitez aussi utiliser uniquement la molette avec cette variable, mettez ipr_SpeedWheel_Config.AddKey en false ! | Disable mouse wheel for weapon selector (you can still use the keyboard to navigate the weapon selector), if you also want to use only the wheel with this variable, set ipr_SpeedWheel_Config.AddKey to false !
+    ipr_SpeedWheel_Config.DisableMWS = false --- Désactiver totalement la molette de la souris pour le sélecteur d'armes (vous pouvez toujours utiliser le clavier pour naviguer dans le sélecteur d'armes), si vous souhaitez aussi utiliser uniquement la molette avec cette variable, mettez ipr_SpeedWheel_Config.AddKey en false ! | Disable mouse wheel for weapon selector (you can still use the keyboard to navigate the weapon selector), if you also want to use only the wheel with this variable, set ipr_SpeedWheel_Config.AddKey to false !
 end
