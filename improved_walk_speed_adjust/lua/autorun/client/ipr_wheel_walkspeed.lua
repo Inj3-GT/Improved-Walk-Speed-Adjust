1e1aa7a14828fa7e0c3de2ffda9af22063d9d371
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2024-12-07 06:19:32 +0100

    v1.4

--- improved_walk_speed_adjust/lua/autorun/client/ipr_wheel_walkspeed.lua
+++ improved_walk_speed_adjust/lua/autorun/client/ipr_wheel_walkspeed.lua
@@ -15,18 +15,19 @@ ipr_SpeedWheel.Bind = {
     ["invnext"] = true,
 }
 
-net.Receive("ipr_swheelsync", function()
+local function ipr_SWheelSync()
     ipr_SpeedWheel.Rotation = net.ReadUInt(ipr_SpeedWheel.NetBits)
-end)
+end
 
-hook.Add("StartCommand", "ipr_MouseWheel_KeyPress", function(ply, cmd)
+local function ipr_SWheelKey()
     if not ipr_SpeedWheel_Config.AddKey[1] then
         return
     end
-    ipr_SpeedWheel.KeyPress = input.IsKeyDown(ipr_SpeedWheel_Config.AddKey.key)
-end)
+    local ipr_Key = input.IsKeyDown(ipr_SpeedWheel_Config.AddKey.key)
+    ipr_SpeedWheel.KeyPress = ipr_Key
+end
 
-local function ipr_WeapSelector(ply, bind, pressed)
+local function ipr_SWheelWeap(ply, bind, pressed)
     if (ipr_SpeedWheel.Bind[bind]) then
         if not ipr_SpeedWheel_Config.DisableMWS then
             if not ipr_SpeedWheel_Config.AddKey[1] then
@@ -44,11 +45,13 @@ local function ipr_Draw_WalkSpeed()
     end
     local ipr_PercentWheel = (ipr_SpeedWheel.Rotation / ipr_SpeedWheel_Config.MaxRotation) * 100
     ipr_PercentWheel = math.Round(ipr_PercentWheel)
-
     local ipr_Wheel = (ipr_SpeedWheel_Config.AddKey[1]) and "+ Molette" or ""
+
     draw.DrawText(ipr_SpeedWheel.Key.. " " ..ipr_Wheel, ipr_SpeedWheel.FontHUD, ipr_SpeedWheel.ScrW / 2, ipr_SpeedWheel.ScrH - 45, color_white, TEXT_ALIGN_CENTER)
     draw.DrawText("Vitesse de marche : " ..ipr_PercentWheel.. "%", ipr_SpeedWheel.FontHUD, ipr_SpeedWheel.ScrW / 2, ipr_SpeedWheel.ScrH - 25, color_white, TEXT_ALIGN_CENTER)
 end
 
-hook.Add("PlayerBindPress", "ipr_MouseWheel_WeapSelector", ipr_WeapSelector)
+net.Receive("ipr_swheelsync", ipr_SWheelSync)
+hook.Add("StartCommand", "ipr_MouseWheel_KeyPress", ipr_SWheelKey)
+hook.Add("PlayerBindPress", "ipr_MouseWheel_WeapSelector", ipr_SWheelWeap)
 hook.Add("HUDPaint", "ipr_MouseWheel_DrawWalkSpeed", ipr_Draw_WalkSpeed)
