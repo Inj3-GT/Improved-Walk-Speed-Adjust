2ca7dfb9fa466a6edbb2228fa32c105afac9b86f
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2024-12-08 03:50:44 +0100

    v1.0

---
+++ improved_walkspeed_adjust/lua/autorun/ipr_walkspeed_loader.lua
@@ -0,0 +1,37 @@
+--- Script By Inj3 
+--- Script By Inj3 
+--- Script By Inj3 
+local Ipr_Cf = file.Find("ipr_walkspeed_sys/configuration/*", "LUA")
+local Ipr_Cl = file.Find("ipr_walkspeed_sys/walkspeed_lua/client/*", "LUA")
+local Ipr_Sh = file.Find("ipr_walkspeed_sys/walkspeed_lua/shared/*", "LUA")
+ipr_SpeedWheel_Config = ipr_SpeedWheel_Config or {}
+
+if (SERVER) then
+     for _, f in pairs(Ipr_Sh) do
+        include("ipr_walkspeed_sys/walkspeed_lua/shared/"..f)
+        AddCSLuaFile("ipr_walkspeed_sys/walkspeed_lua/shared/"..f)
+     end
+     for _, f in pairs(Ipr_Cf) do
+         include("ipr_walkspeed_sys/configuration/"..f)
+         AddCSLuaFile("ipr_walkspeed_sys/configuration/"..f)
+     end
+
+     local Ipr_Sv = file.Find("ipr_walkspeed_sys/walkspeed_lua/server/*", "LUA")
+     for _, f in pairs(Ipr_Sv) do
+         include("ipr_walkspeed_sys/walkspeed_lua/server/"..f)
+     end
+     for _, f in pairs(Ipr_Cl) do
+         AddCSLuaFile("ipr_walkspeed_sys/walkspeed_lua/client/"..f)
+     end
+     print("Improved Join/Leave System By Inj3 - Loaded")
+ else
+     for _, f in pairs(Ipr_Sh) do
+        include("ipr_walkspeed_sys/walkspeed_lua/shared/"..f)
+     end
+     for _, f in pairs(Ipr_Cf) do
+         include("ipr_walkspeed_sys/configuration/"..f)
+     end
+     for _, f in pairs(Ipr_Cl) do
+         include("ipr_walkspeed_sys/walkspeed_lua/client/"..f)
+     end
+end
